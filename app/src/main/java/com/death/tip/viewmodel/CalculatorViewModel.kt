80ace66e7ef83a3cc9f3f3efca4ed0cd58af14a4
deathcode <carotkut12@gmail.com>
2019-01-27 18:56:26 +0530

    View hooked up with ViewModel

--- app/src/main/java/com/death/tip/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/death/tip/viewmodel/CalculatorViewModel.kt
@@ -1,26 +1,39 @@
 package com.death.tip.viewmodel
 
-import android.util.Log
+import android.app.Application
 import androidx.databinding.BaseObservable
+import com.death.tip.R
 import com.death.tip.model.Calculator
 import com.death.tip.model.TipCalculation
 
-class CalculatorViewModel(private val calculator:Calculator = Calculator()) : BaseObservable() {
+class CalculatorViewModel(val app: Application, private val calculator:Calculator = Calculator()) : BaseObservable() {
 
     var inputCheckAmount = ""
     var inputTipPercentage = ""
-    var tipCalculation = TipCalculation()
+
+    //var tipCalculation = TipCalculation()
+
+    var outputCheckAmount = ""
+    var outputTipAmount = ""
+    var outputTotalAmount = ""
+
+    init {
+        updateOutputs(TipCalculation())
+    }
+
+    private fun updateOutputs(tc: TipCalculation){
+        outputCheckAmount = app.getString(R.string.dollar_amount, tc.checkAmount)
+        outputTipAmount = app.getString(R.string.dollar_amount, tc.tipAmount)
+        outputTotalAmount = app.getString(R.string.dollar_amount, tc.grandTotal)
+    }
 
     fun calculateTip(){
-
-        Log.e("ViewModel", "calculateTipInvoked")
 
         val checkAmount = inputCheckAmount.toDoubleOrNull()
         val tipPct = inputTipPercentage.toIntOrNull()
 
         if(checkAmount!=null && tipPct!=null){
-            Log.e("ViewModel","CheckAmount: $checkAmount, TipPercentage: $tipPct")
-            tipCalculation = calculator.calculateTip(checkAmount, tipPct)
+            updateOutputs(calculator.calculateTip(checkAmount, tipPct))
             clearInputs()
         }
 
--- app/src/test/java/com/death/tip/viewmodel/CalculatorViewModelTest.kt
+++ app/src/test/java/com/death/tip/viewmodel/CalculatorViewModelTest.kt
@@ -1,8 +1,11 @@
 package com.death.tip.viewmodel
 
+import android.app.Application
+import com.death.tip.R
 import com.death.tip.model.Calculator
 import com.death.tip.model.TipCalculation
 import junit.framework.Assert.assertEquals
+import net.bytebuddy.description.type.TypeDescription
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers
@@ -17,10 +20,18 @@ class CalculatorViewModelTest {
     @Mock
     lateinit var mockCalculator: Calculator
 
+    @Mock
+    lateinit var application: Application
+
     @Before
     fun setup(){
         MockitoAnnotations.initMocks(this)
-        calculatorViewModel = CalculatorViewModel(mockCalculator)
+        stubResource(0.0, "$0.00")
+        calculatorViewModel = CalculatorViewModel(application,mockCalculator)
+    }
+
+    private fun stubResource(given: Double, returnStub:String){
+        `when`(application.getString(R.string.dollar_amount, given)).thenReturn(returnStub)
     }
 
     @Test
@@ -31,15 +42,32 @@ class CalculatorViewModelTest {
         val stub = TipCalculation(checkAmount = 10.00,tipAmount = 1.5, grandTotal = 11.5)
         `when`(mockCalculator.calculateTip(10.00, 15)).thenReturn(stub)
 
+
+        stubResource(10.0, "$10.00")
+        stubResource(1.5, "$1.50")
+        stubResource(11.5, "$11.50")
+
         calculatorViewModel.calculateTip()
 
-        assertEquals(stub, calculatorViewModel.tipCalculation)
+        assertEquals("$10.00", calculatorViewModel.outputCheckAmount)
+        assertEquals("$1.50", calculatorViewModel.outputTipAmount)
+        assertEquals("$11.50", calculatorViewModel.outputTotalAmount)
     }
 
     @Test
     fun badCheckAmount(){
         calculatorViewModel.inputCheckAmount = ""
         calculatorViewModel.inputTipPercentage = "15"
+
+        calculatorViewModel.calculateTip()
+
+        verify(mockCalculator, never()).calculateTip(ArgumentMatchers.anyDouble(), ArgumentMatchers.anyInt())
+    }
+
+    @Test
+    fun badTipAmount(){
+        calculatorViewModel.inputCheckAmount = "10.00"
+        calculatorViewModel.inputTipPercentage = ""
 
         calculatorViewModel.calculateTip()
 
