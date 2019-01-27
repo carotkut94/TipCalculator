eee9d0e29d0b6a739b77fd3320a41cafcc377ce4
deathcode <carotkut12@gmail.com>
2019-01-27 18:31:16 +0530

    Added two way binding for input fields

--- app/src/main/java/com/death/tip/view/MainActivity.kt
+++ app/src/main/java/com/death/tip/view/MainActivity.kt
@@ -2,12 +2,17 @@ package com.death.tip.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.death.tip.R
+import com.death.tip.viewmodel.CalculatorViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: com.death.tip.databinding.ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.vm = CalculatorViewModel()
     }
 }
--- app/src/main/java/com/death/tip/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/death/tip/viewmodel/CalculatorViewModel.kt
@@ -1,21 +1,35 @@
 package com.death.tip.viewmodel
 
+import android.util.Log
+import androidx.databinding.BaseObservable
 import com.death.tip.model.Calculator
 import com.death.tip.model.TipCalculation
 
-class CalculatorViewModel(private val calculator:Calculator = Calculator()) {
+class CalculatorViewModel(private val calculator:Calculator = Calculator()) : BaseObservable() {
 
     var inputCheckAmount = ""
     var inputTipPercentage = ""
     var tipCalculation = TipCalculation()
 
     fun calculateTip(){
+
+        Log.e("ViewModel", "calculateTipInvoked")
+
         val checkAmount = inputCheckAmount.toDoubleOrNull()
         val tipPct = inputTipPercentage.toIntOrNull()
 
         if(checkAmount!=null && tipPct!=null){
+            Log.e("ViewModel","CheckAmount: $checkAmount, TipPercentage: $tipPct")
             tipCalculation = calculator.calculateTip(checkAmount, tipPct)
+            clearInputs()
         }
+
+    }
+
+    private fun clearInputs() {
+        inputTipPercentage = ""
+        inputCheckAmount = ""
+        notifyChange()
     }
 
 }
